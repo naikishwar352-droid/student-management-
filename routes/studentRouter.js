4bd7519681a1c34db21346629c740f15daf4938b
Naikishwar <naikishwar352@gmail.com>
2026-03-06 18:51:20 +0530

    added delete api

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -33,5 +33,14 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+// DELETE user
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
